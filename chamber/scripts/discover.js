e5f43ebc678a2024175a889516df869b855664c9
meisakae <sakameeeeii17@gmail.com>
2025-08-06 23:19:10 -0600

    week05

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -41,3 +41,15 @@ if(!lastVisit) {
 }
 
 localStorage.setItem("lastVisit", now);
+
+const navbutton = document.querySelector('#ham-btn');
+const navlinks = document.querySelector('#nav-bar');
+//Toggle the show class off and on
+
+navbutton.addEventListener('click', ()=> {
+    navbutton.classList.toggle('show');
+    navlinks.classList.toggle('show');
+});
+
+document.getElementById('current-year').textContent = new Date().getFullYear();
+document.getElementById('last-Modified').textContent = document.lastModified;
