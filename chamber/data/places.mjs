ddbbf2270bf3fb0aedcde02a26edda6b636805db
meisakae <sakameeeeii17@gmail.com>
2025-08-06 23:01:25 -0600

    week05

---
+++ chamber/data/places.mjs
@@ -0,0 +1,50 @@
+export const places = [
+  {
+    "name": "Fushimi Inari Taisha",
+    "address": "68 Fukakusa Yabunouchicho, Fushimi‑ku, Kyoto, Kyoto Prefecture, Japan",
+    "description": "A famous shrine known for its endless rows of vibrant red torii gates. It enshrines the god of rice and prosperity, Inari.",
+    "image": "images/fushimi.webp"
+  },
+  {
+    "name": "Lake Ashi (Hakone)",
+    "address": "Motohakone, Hakone-machi, Ashigarashimo District, Kanagawa Prefecture 250‑0522, Japan",
+    "description": "A beautiful caldera lake with views of Mount Fuji. Known for sightseeing cruises, hot springs, and the Hakone Shrine.",
+    "image": "images/hakone.webp"
+  },
+  {
+    "name": "Nikkō Tōshō‑gū",
+    "address": "2301 Sannai, Nikkō, Tochigi Prefecture 321‑1431, Japan",
+    "description": "A lavish shrine dedicated to Tokugawa Ieyasu, famous for intricate carvings like the three wise monkeys and Sleeping Cat.",
+    "image": "images/nikko.webp"
+  },
+  {
+    "name": "Tōdai‑ji Temple",
+    "address": "406‑1 Zōshichō, Nara City, Nara Prefecture 630‑8587, Japan",
+    "description": "A massive wooden temple housing one of the largest bronze Buddha statues in the world, located in Nara Park.",
+    "image": "images/todaiji.webp"
+  },
+  {
+    "name": "Shirakawa‑go",
+    "address": "2499 Shirakawa‑Village, Ono District, Gifu Prefecture, Japan",
+    "description": "A UNESCO World Heritage site known for traditional gassho-style houses with steep thatched roofs, especially picturesque in winter.",
+    "image": "images/shirakawago.webp"
+  },
+  {
+    "name": "Itsukushima Shrine (Miyajima)",
+    "address": "1‑1 Miyajimacho, Hatsukaichi City, Hiroshima Prefecture 739‑0588, Japan",
+    "description": "A sacred shrine with a floating red torii gate that appears to rise from the sea during high tide. A UNESCO World Heritage Site.",
+    "image": "images/itshukushima.webp"
+  },
+  {
+    "name": "Lake Kawaguchi",
+    "address": "Fuji‑Kawaguchiko‑machi, Minamitsuru‑gun, Yamanashi Prefecture 401‑0300, Japan",
+    "description": "One of the Fuji Five Lakes, offering iconic views of Mount Fuji, cherry blossoms in spring, and autumn colors in fall.",
+    "image": "images/kawaguchi.webp"
+  },
+  {
+    "name": "Okinawa Islands",
+    "address": "Okinawa Prefecture, Japan",
+    "description": "A subtropical island chain known for turquoise waters, coral reefs, beautiful beaches, and distinct Ryukyu culture.",
+    "image": "images/okinawa.webp"
+  }
+]
---
+++ chamber/scripts/discover.js
@@ -0,0 +1,43 @@
+import { places } from '../data/places.mjs';
+console.log(places)
+
+const main = document.querySelector('.discover-grid');
+
+places.forEach((place,index) => {
+    const card = document.createElement('section');
+    card.classList.add('card', `card${index + 1}`);
+
+    card.innerHTML = `
+    <h2>${place.name}</h2>
+    <figure>
+        <Image src="${place.image}" alt="${place.name}" loding="lazy" width="300" height="200"
+    </figure>
+    <address>${place.address}</address>
+    <p>${place.description}</p>
+    <button>Learn More</button>
+    `;
+
+    main.appendChild(card);
+})
+
+const visitMsg = document.getElementById("visit-message");
+const lastVisit = localStorage.getItem("lastVisit");
+const now = Date.now();
+const msToDays = 86400000;
+
+if(!lastVisit) {
+    visitMsg.textContent = "Welcome! Let us know if you have any questions.";
+} else {
+    const days = Math.floor((now - Number(lastVisit)) / msToDays);
+
+    if(days < 1) {
+        visitMsg.textContent = "Back so soon! Awesome!";
+    } else if (days === 1) {
+        visitMsg.textContent = "You last visited 1 days ago.";
+    } else {
+        visitMsg.textContent = `You last visited ${days} days ago`;
+    }
+
+}
+
+localStorage.setItem("lastVisit", now);
